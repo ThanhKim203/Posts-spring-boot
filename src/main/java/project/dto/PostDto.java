f2c3a51b5b283c8a6cb1e2e6cb094748c377d0e3
ds-thanh-kim <thanh-kim@dimage.co.jp>
2024-02-20 15:26:28 +0700

    add-postDto

--- src/main/java/project/dto/PostDto.java
+++ src/main/java/project/dto/PostDto.java
@@ -1,4 +1,59 @@
 package project.dto;
 
+import project.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
 public class PostDto {
+    @NotNull(message = "Title shouldn't be null")
+    @NotBlank(message = "Title shouldn't be Blank")
+    @Size(max = 50, message = "Title shouldn't has more than 50 letters")
+    private String title;
+    private String description;
+    private String link;
+    private LocalDateTime registerDate;
+    private User user;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
